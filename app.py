7fd413e1205ee42437644204b65bc699a1ca784c
shweth <shwet@shweth05.com>
2026-03-15 22:37:25 +0530

    Update DB config for Render deployment support

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ jwt = JWTManager(app)
 cipher_suite = Fernet(b'ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg=')
 
 DATABASE_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "123456",
-    "database": "qr_attendence"
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "user": os.environ.get("DB_USER", "root"),
+    "password": os.environ.get("DB_PASSWORD", "123456"),
+    "database": os.environ.get("DB_NAME", "qr_attendence"),
+    "port": int(os.environ.get("DB_PORT", 3306))
 }
 
 def get_db():
